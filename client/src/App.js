f42331779098734b1fb6fee2746b2346adb43e62
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2022-01-17 22:02:49 -0800

    clean up react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { useState, useEffect } from "react";
 
@@ -24,22 +23,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={postHandler}>post</button>
-        <button onClick={getHandler}>get</button>
-      </header>
+      <button onClick={postHandler}>post</button>
+      <button onClick={getHandler}>get</button>
     </div>
   );
 }
