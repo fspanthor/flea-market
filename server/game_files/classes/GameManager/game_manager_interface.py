be537397403e24af194d1c7f1134c2bd4dc72f9f
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2022-03-19 14:42:31 -0700

    clean up game_manager_interface

--- server/game_files/classes/GameManager/game_manager_interface.py
+++ server/game_files/classes/GameManager/game_manager_interface.py
@@ -1,20 +1,8 @@
 from flask import jsonify
 
 
-def retrieve_game_state(game_instance):
-    return jsonify(game_instance.game_manager.game_state())
-
-
-def retrieve_game_state(game_instance):
-    return jsonify(game_instance.game_manager.get_day())
-
-
 def buy_sell_jet(game_instance, params):
     return jsonify(game_instance.game_manager.buy_sell_jet(params['key']))
-
-
-def jet(game_instance, params):
-    return jsonify(game_instance.game_manager.jet(params['key']))
 
 
 def new_game(game_instance):
@@ -27,8 +15,6 @@ def get_day(game_instance):
 
 game_manager_interface = {
     'BUY_SELL_JET': buy_sell_jet,
-    'RETRIEVE_GAME_STATE': retrieve_game_state,
-    'JET': jet,
     'NEW_GAME': new_game,
     'GET_DAY': get_day
 }
