00bb59cba1ab9e68b1e487b7d54937623ba33446
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2022-05-08 12:23:16 -0700

    initial db work

---
+++ server/db/pymongo.py
@@ -0,0 +1,42 @@
+from bson import ObjectId
+from pymongo import MongoClient
+import pymongo
+import urllib.parse
+from db_keys import user, password, clusterId, dbId, dbName, collectionName
+
+CONNECTION_STRING = f"mongodb+srv://{urllib.parse.quote_plus(user)}:{urllib.parse.quote_plus(password)}@{clusterId}/{dbId}"
+client = MongoClient(CONNECTION_STRING)
+db = client[dbName]
+collection = db[collectionName]
+
+
+def test():
+
+    # python -c 'import pymongo_insert; pymongo_insert.test()'
+
+    # to insert stuff:
+    # document = {"_id": ObjectId(), "user_name": "hi", "score": 2000}
+    # collection.insert_one(document)
+
+    # to query top scores:
+
+    scores = list(collection.find({}, {'_id': 0}).sort(
+        'score', pymongo.DESCENDING).limit(2))
+
+    print(scores)
+
+
+def insert_high_score(name, score):
+    document = {"_id": ObjectId(), "user_name": name, "score": score}
+    collection.insert_one(document)
+    return
+
+# retrieve number of scores as indicated
+# find all records, use a projection to exclude the _id field from return
+# query results come back as a pymongo.cursor.Cursor and we will turn it into a list of dictionaries with list
+
+
+def retrieve_high_scores(number_of_scores):
+    scores = list(collection.find({}, {'_id': 0}).sort(
+        'score', pymongo.DESCENDING).limit(number_of_scores))
+    return scores
