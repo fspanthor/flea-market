b22b771bf86d31a8acef2c57e6cdf9f91d027c0d
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2022-05-21 22:27:35 -0700

    add db env variables

--- server/game_files/db/test_keys.py
+++ server/game_files/db/test_keys.py
@@ -1,6 +1,8 @@
-user = 'z'
-password = 'z'
-clusterId = 'cluster0.04qbn.mongodb.net'
-dbId = 'z'
-dbName = 'z'
-collectionName = 'z'
+import os
+
+user = os.environ['DB_USER']
+password = os.environ['DB_PASSWORD']
+clusterId = os.environ['DB_CLUSTER_ID']
+dbId = os.environ['DB_ID']
+dbName = os.environ['DB_NAME']
+collectionName = os.environ['DB_COLLECTION_NAME']
